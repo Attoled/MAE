82ba6ab3989ae86def0a0777b4045da56f48d34d
Atolled <denilson.junior@poli.ufrj.br>
2024-02-27 13:41:51 -0300

    initial changes

--- maeAPI/mae/views.py
+++ maeAPI/mae/views.py
@@ -1,9 +1,15 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
-def testar_bancos(request, *args, **kwargs):
-    return JsonResponse({"teste":"voce conseguiu ajsdajsdasd"})
 
+def testar_bancos(request, *args, **kwargs):
+    return JsonResponse({"teste": "voce conseguiu ajsdajsdasd"})
+
+
+@api_view(["GET"])
 def testar_api(request, *args, **kwargs):
-    return JsonResponse({"api":"maravilha garotoo"})
+    # request.GET "params"
+
+    return JsonResponse({"api": "maravilha garotoo"})
